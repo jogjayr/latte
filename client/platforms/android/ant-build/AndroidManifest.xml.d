dfc0479ce7ae7f090908e5748bf5e385c7c853e4
Jayraj Jog <jayraj@bittorrent.com>
2015-03-07 12:47:40 -0800

    Add initial commit of phonegap app

---
+++ client/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/jayraj/personal/latte factor/client/platforms/android/AndroidManifest.xml \
---
+++ client/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/classes.dex : \
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/classes/com/phonegap/helloworld/BuildConfig.class \
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/classes/com/phonegap/helloworld/CordovaApp.class \
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/classes/com/phonegap/helloworld/R$attr.class \
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/classes/com/phonegap/helloworld/R$drawable.class \
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/classes/com/phonegap/helloworld/R$string.class \
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/classes/com/phonegap/helloworld/R$xml.class \
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/classes/com/phonegap/helloworld/R.class \
+/Users/jayraj/personal/latte factor/client/platforms/android/ant-build/dexedLibs/classes-b95d22b61b7548885408541dbf673f43.jar \
